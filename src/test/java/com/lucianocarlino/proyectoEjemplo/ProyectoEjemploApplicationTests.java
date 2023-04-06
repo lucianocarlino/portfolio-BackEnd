e8a5f32408ecff5a32764e22552f641cc7d1b399
luciano <luc02@live.com>
2023-04-06 12:53:38 -0300

    primer commit

---
+++ src/test/java/com/lucianocarlino/proyectoEjemplo/ProyectoEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianocarlino.proyectoEjemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
